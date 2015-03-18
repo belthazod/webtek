1140c9730fb5486b88df7ac16c7807f7223e3be0
Jolas Burayag <jolasburayag@gmail.com>
2015-03-19 00:21:52 +0800

    restructuring of html; added localstorage functionalities and more

--- scripts/create-class.js
+++ scripts/create-class.js
@@ -18,11 +18,75 @@ var myclass = {
 };
 var studens = [];
 
+//populates student-selection element
+function initializeStudentSelector() {
+	var selector = document.getElementById("student-selection");
+	var eStudents = getExistingStudents();
+	for(var i in eStudents) {
+		var opt = document.createElement("option");
+		opt.value = i;
+		opt.text = eStudents[i];
+
+		selector.add(opt);
+	}
+
+	var addSelected = document.getElementById("addselected-btn");
+	addSelected.addEventListener('click', function(){addSelectedEvent()});
+	function addSelectedEvent() {
+		alert(eStudents(selector.selectedIndex));
+	}
+}
+
+//retrieves eStudents from localstorage
 function getExistingStudents() {
-	var eStudentsSelector = document.getElementById('student-selection');
-	var estudents = JSON.parse(localStorage.getItem('eStudents'));
+	return JSON.parse(localStorage.getItem('eStudents'));
 }
 
-function finalize() {
-
+//clears localStorage 'eStudents'
+function emptyStudents() {
+	localStorage.removeItem('eStudents');
 }
+
+//adds test data to localstorage 'eStudents'
+function addTestData() {
+	var eStudents = getExistingStudents();
+	var testStudents = ['Burayag, Jolas G.'];
+	var trigger = false;
+	if (eStudents != null) {
+		for (var i in testStudents) {
+			for (var j in eStudents.length) {
+
+				if (testStudents[j] == eStudents[i]) {
+					trigger = true;
+				}
+			}
+		}
+		if(!trigger) {localStorage.setItem('eStudents', JSON.stringify(eStudents))};
+	} else {
+		localStorage.setItem('eStudents', JSON.stringify(testStudents));
+	}
+	
+}
+
+//prints localstorage 'eStudents' value
+function printLocal() {
+	var eStudents = getExistingStudents();
+	var students = "";
+	for (var i = eStudents.length - 1; i >= 0; i--) {
+		students = students+" "+eStudents[i];
+	};
+	alert(students);
+}
+
+function main() {
+	document.addEventListener('DOMContentLoaded', function () {
+	    console.log('DOM ready!');
+	});
+
+	emptyStudents();
+	addTestData();
+	//printLocal();
+	initializeStudentSelector()	
+}
+
+main();
