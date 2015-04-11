78fe87708c6b5bd2c91f6b4384deaf1d47bb9fc5
belthazod <belthazod@gmail.com>
2015-04-11 08:06:43 +0800

    Stud Rec now working Modified sample data structure @ attendanceChecker.html

--- scripts/checkAttendance.js
+++ scripts/checkAttendance.js
@@ -1,4 +1,4 @@
-function universityClass( classcode, descriptiveTitle, room, sched, numOfStud, username, students){
+function universityClass( classcode, descriptiveTitle, room, sched, numOfStud, username, students, classDates){
 	this.classcode = classcode;
 	this.descriptiveTitle = descriptiveTitle;
 	this.room = room;
@@ -6,18 +6,21 @@ function universityClass( classcode, descriptiveTitle, room, sched, numOfStud, u
 	this.numOfStud = numOfStud;
 	this.username = username;
 	this.students = students;
+	this.classDates = classDates;
 }
 
-function Student(stud_id, last_name, first_name, middle_initial, x_coordinate, y_coordinate, studstatus, username, records){
+function Student(stud_id, last_name, first_name, middle_initial, x_coordinate, y_coordinate, studstatus, username, records, tardinessCount, absenceCount){
 	this.stud_id = stud_id;
 	this.last_name = last_name;
 	this.first_name = first_name;
-	this.middle_inital = middle_initial;
+	this.middle_initial = middle_initial;
 	this.x_coordinate = x_coordinate;
 	this.y_coordinate = y_coordinate;
 	this.studstatus = studstatus;
 	this.username = username;
 	this.records = records;
+	this.tardinessCount = tardinessCount;
+	this.absenceCount =absenceCount;
 }
 function Record(date, type){
 	this.date = date;
